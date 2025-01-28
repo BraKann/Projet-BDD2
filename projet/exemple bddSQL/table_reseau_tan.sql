6ca4d39fe2df212448f2081cc35598bc109f462c
Gwendoline19 <141649960+Gwendoline19@users.noreply.github.com>
2025-01-28 12:52:07 +0100

    maj des DF

--- projet/exemple bddSQL/table_reseau_tan.sql
+++ projet/exemple bddSQL/table_reseau_tan.sql
@@ -27,23 +27,28 @@ CREATE TABLE RESEAUTAN (
     --PRIMARY KEY (line_id, station_id, vehicle_id)
     --CONSTRAINT pk_reseauxtan PRIMARY KEY(ligne_id,arret_id,vehicle_id)
 
-    --DF : ligne_id → ligne_nom, ligne_type
-    -- arret_id → arret_nom
-    -- vehicule_id → vehicule_type, capacite, carburant_type, accessibilite
-    -- line_id, arret_id → arret_nom
-    -- ligne_id, vehicule_id → vehicule_type, capacite, carburant_type, accessibilite
-    -- capacite -> vehicule_type (mais changera si on met le model des vehicule)
+    --DF : 
+    -- ligne_id → ligne_nom, ligne_type;
+    -- arret_id → arret_nom;
+    -- line_id, arret_id → arret_nom;
+    -- capacite -> vehicule_type; (mais changera si on met le model des vehicule)
 
+    -- ligne_id, arret_id, vehicule_id → vehicule_type, capacite, carburant_type, accessibilite;
+    -- ligne_id, vehicule_id → vehicule_type, capacite, carburant_type, accessibilite;
+    -- arret_id, vehicule_id → vehicule_type, capacite, carburant_type, accessibilite;
+    -- vehicule_id → vehicule_type, capacite, carburant_type, accessibilite;
     
-    --kilometrage
-    --annee mise en service
-    --heure service de la ligne
-    --nombre de vehicule sur la ligne
-    --tranche de passage
-    --Les conducteurs / employés
-    --marque vehicule(change les vitesse et capacite)
-    --vitesse max et moyenne vehicule
-    --mode nocturne qui permet un arret hors des arret de base sur certaine ligne
+
+    -- Potentielles améliorations pour ajout des attributs :
+    -- kilometrage
+    -- annee mise en service
+    -- heure service de la ligne
+    -- nombre de vehicule sur la ligne
+    -- tranche de passage
+    -- Les conducteurs / employés
+    -- marque vehicule(change les vitesse et capacite)
+    -- vitesse max et moyenne vehicule
+    -- mode nocturne qui permet un arret hors des arret de base sur certaine ligne
 );
 
 spool off
