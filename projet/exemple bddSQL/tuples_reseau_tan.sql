174c88f34a19d6868785d55af88e949aa5e696a3
BraKann <80362852+BraKann@users.noreply.github.com>
2025-02-01 12:48:54 +0100

    Update tuples_reseau_tan.sql

--- projet/exemple bddSQL/tuples_reseau_tan.sql
+++ projet/exemple bddSQL/tuples_reseau_tan.sql
@@ -17,11 +17,12 @@ INSERT INTO RESEAUTAN (ligne_id, ligne_nom, ligne_type, arret_id, arret_nom, veh
     (1,     'Ligne 1',            'Rail',         3,       'Hôtel Dieu',         102,   'Tramway',      250 ,  'Électrique',    'Rampe d’accès'),
     (2,     'Ligne 2',            'Rail',         2,       'Beaujoire',          102,   'Tramway',      250 ,  'Électrique',    'Rampe d’accès'),
     (3,     'C5',                 'Route',        3,       'Hôtel Dieu',         201,   'Bus',          50,    'Gaz_Naturel',   'Rampe d’accès'),
-    (9,     '12',                 'Route',        9,        'Université',        202,   'Bus',          50,    'Gaz_Naturel',   'Rampe d’accès'); 
     (4,     'N1',                 'Fluviales',    4,       'Gare Maritime',      301,   'Navibus',      95,    'Diesel',        'Rampe d’accès'),
     (4,     'N1',                 'Fluviales',    5,       'Trentemoult',        301,   'Navibus',      95,    'Diesel',        'Rampe d’accès'),
     (5,     'N3',                 'Fluviales',    6,       'Gare Sud',           302,   'Navibus',      95,    'Hydrogène',     'Rampe d’accès'),
     (6,     'Ligne 4',            'Route',        7,       'Hôtel de Région',    401,   'BusWay',       150,   'Électrique',    'Rampe d’accès, WiFi'),
-    (10,    'Ligne 5',            'Route',        10,      'Hangar a Banane',    402,   'BusWay',       150,   'Électrique',    'Rampe d’accès, WiFi'),
     (7,     'Loire N2',           'Fluviales',    8,       'Île de Nantes',      303,   'Navibus',      95,    'Électrique',    'Rampe d’accès'),
     (8,     'Déplacement dédié',  'Route',        NULL,     NULL,                501,   'ProxiTan',     4,     'Diesel',        'Rampe d’accès, Espace fauteuil,Ascenseur'),
+    (9,     '12',                 'Route',        9,        'Université',        202,   'Bus',          50,    'Gaz_Naturel',   'Rampe d’accès'); 
+    (10,    'Ligne 5',            'Route',        10,      'Hangar a Banane',    402,   'BusWay',       150,   'Électrique',    'Rampe d’accès, WiFi'),
+    
