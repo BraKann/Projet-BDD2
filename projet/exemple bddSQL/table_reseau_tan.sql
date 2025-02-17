a52a342dfae0031f3ee2b7925b98206dbb65006e
Christopher <christopher.chiaberge@etu.univ-nantes.fr>
2025-02-18 00:22:22 +0100

    Ajout des tables obtenue par la normalisation dite de décomposition

--- projet/exemple bddSQL/table_reseau_tan.sql
+++ projet/exemple bddSQL/table_reseau_tan.sql
@@ -6,52 +6,51 @@ prompt *************************************************************
 prompt ******************** DROP TABLE *****************************
 prompt *************************************************************
 
-DROP TABLE RESEAUTAN CASCADE CONSTRAINTS;
+DROP TABLE RESEAUX CASCADE CONSTRAINTS;
+DROP TABLE VEHICULE CASCADE CONSTRAINTS;
+DROP TABLE LIGNE CASCADE CONSTRAINTS;
+DROP TABLE ARRET CASCADE CONSTRAINTS;
+                                        
 
 prompt *************************************************************
 prompt ******************** CREATE TABLE ***************************
 prompt *************************************************************
 
-CREATE TABLE RESEAUTAN (
+CREATE TABLE LIGNE (
     ligne_id NUMBER,
     ligne_nom VARCHAR(255),
     ligne_type VARCHAR(50),
-    arret_id NUMBER,
-    arret_nom VARCHAR(255),
+
+    PRIMARY KEY (ligne_id)
+    --CONSTRAINT pk_reseauxtan PRIMARY KEY(ligne_id,arret_id,vehicle_id)
+);
+
+CREATE TABLE VEHICULE (
     vehicule_id NUMBER,
     vehicule_type VARCHAR(50),
     capacite NUMBER,
     carburant VARCHAR(50),
     accessibilite VARCHAR(255),
 
-    --PRIMARY KEY (line_id, station_id, vehicle_id)
+    PRIMARY KEY (vehicule_id)
     --CONSTRAINT pk_reseauxtan PRIMARY KEY(ligne_id,arret_id,vehicle_id)
+);
 
-    --DF : 
-    -- ligne_id → ligne_nom, ligne_type;
-    -- arret_id → arret_nom;
-    -- ligne_id, arret_id → arret_nom;
-    -- capacite → vehicule_type; (mais changera si on met le model des vehicule)
-    -- ligne_nom → ligne_id, ligne_type;
-    -- ligne_id, arret_id, vehicule_id → vehicule_type, capacite, carburant, accessibilite;
-    -- ligne_id, vehicule_id → vehicule_type, capacite, carburant, accessibilite;
-    -- arret_id, vehicule_id → vehicule_type, capacite, carburant, accessibilite;
-    -- vehicule_id → vehicule_type, capacite, carburant, accessibilite;
-    
-    -- arret_id → ligne_type (elle ne répresente pas la realité)
-    -- arret_nom → arret_id (on ne la garde pas car en réalité il y a plusieurs arrêts qui ont le même nom)
-    
+CREATE TABLE ARRET (
+    arret_id NUMBER,
+    arret_nom VARCHAR(255),
 
-    -- Potentielles améliorations pour ajout des attributs :
-    -- kilometrage
-    -- annee mise en service
-    -- heure service de la ligne
-    -- nombre de vehicule sur la ligne
-    -- tranche de passage
-    -- Les conducteurs / employés
-    -- marque vehicule(change les vitesse et capacite)
-    -- vitesse max et moyenne vehicule
-    -- mode nocturne qui permet un arret hors des arret de base sur certaine ligne
+    PRIMARY KEY (arret_id)
+    --CONSTRAINT pk_reseauxtan PRIMARY KEY(ligne_id,arret_id,vehicle_id)
+);
+
+CREATE TABLE RESEAUX (
+    ligne_id NUMBER,
+    arret_id NUMBER,
+    vehicule_id NUMBER,
+
+    PRIMARY KEY (ligne_id, arret_id, vehicule_id)
+    --CONSTRAINT pk_reseauxtan PRIMARY KEY(ligne_id,arret_id,vehicle_id)
 );
 
 spool off
