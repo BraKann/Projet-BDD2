aa9a498fac615feb7ee3d6e94050f86cb72269b9
Gwendoline19 <141649960+Gwendoline19@users.noreply.github.com>
2025-03-20 12:11:07 +0100

    Update vues.sql

--- Projet-BDD2-main/sql/vues.sql
+++ Projet-BDD2-main/sql/vues.sql
@@ -1,31 +1,31 @@
 -- vue pour récupérer les lignes complètes
 CREATE VIEW Vue_Lignes_Completes AS
-SELECT l.ligne_id, l.nom AS ligne_nom, l.type AS ligne_type, a.nom AS arret_nom, v.type AS vehicule_type, v.carburant
+SELECT l.ligne_id, l.ligne_nom AS LigneNom, l.ligne_type AS Ltype, a.arret_nom AS ArretNom, v.vehicule_type AS Vtype
 FROM LIGNE l
-JOIN RESEAUX r ON l.ligne_id = r.id_ligne
-JOIN ARRET a ON r.id_arret = a.arret_id
-JOIN VEHICULE v ON r.id_vehicule = v.vehicule_id;
+JOIN RESEAUX r ON l.ligne_id = r.ligne_id
+JOIN ARRET a ON r.arret_id = a.arret_id
+JOIN VEHICULE v ON r.vehicule_id = v.vehicule_id;
 
 --vue pour récuperer les lignes sur lesquelles circulent des bus
 CREATE VIEW Vue_Lignes_Bus AS
-SELECT DISTINCT l.ligne_id, l.nom AS ligne_nom, l.type AS ligne_type
+SELECT DISTINCT l.ligne_id, l.ligne_nom AS ligneNom, l.ligne_type AS Ltype
 FROM LIGNE l
-JOIN RESEAUX r ON l.ligne_id = r.id_ligne
-JOIN VEHICULE v ON r.id_vehicule = v.vehicule_id
-WHERE v.type = 'Bus';
+JOIN RESEAUX r ON l.ligne_id = r.ligne_id
+JOIN VEHICULE v ON r.vehicule_id = v.vehicule_id
+WHERE v.vehicule_type = 'Bus';
 
 --vue pour récuperer les lignes sur lesquelles circulent des tramway
 CREATE VIEW Vue_Lignes_Tramway AS
-SELECT DISTINCT l.ligne_id, l.nom AS ligne_nom, l.type AS ligne_type
+SELECT DISTINCT l.ligne_id, l.ligne_nom AS ligneNom, l.ligne_type AS Ltype
 FROM LIGNE l
-JOIN RESEAUX r ON l.ligne_id = r.id_ligne
-JOIN VEHICULE v ON r.id_vehicule = v.vehicule_id
-WHERE v.type = 'Tramway';
+JOIN RESEAUX r ON l.ligne_id = r.ligne_id
+JOIN VEHICULE v ON r.vehicule_id = v.vehicule_id
+WHERE v.vehicule_type = 'Tramway';
 
 --vue pour récuperer les lignes qui passent par l'arrêt 'Commerce'
 CREATE VIEW Vue_Lignes_Commerce AS
-SELECT DISTINCT l.ligne_id, l.nom AS ligne_nom, l.type AS ligne_type
+SELECT DISTINCT l.ligne_id, l.ligne_nom AS ligne_nom, l.ligne_type AS Ltype
 FROM LIGNE l
-JOIN RESEAUX r ON l.ligne_id = r.id_ligne
-JOIN ARRET a ON r.id_arret = a.arret_id
-WHERE a.nom = 'Commerce';
+JOIN RESEAUX r ON l.ligne_id = r.ligne_id
+JOIN ARRET a ON r.arret_id = a.arret_id
+WHERE a.arret_nom = 'Commerce';
